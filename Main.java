009332c46dbeb01b3b7fd4d4c3e8b6a38e096214
Sivabala-sba <87258592+Sivabala-sba@users.noreply.github.com>
2021-07-11 18:41:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         //My first program in Java
+        //This program was done on 11th july, 2021
         System.out.print("Hello world");
     }
 }
