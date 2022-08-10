b8c3c62f7269c1d780f1bec4faef631b977039b0
Sivabala-sba <87258592+Sivabala-sba@users.noreply.github.com>
2022-08-10 18:30:05 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,8 @@
-package com.company;
+package com.java.examples;
 
-import java.util.Scanner;
-//This program will get a input from the user and will give the output as whether the input number is positive or negative.
 public class Main {
 
     public static void main(String[] args) {
-        Scanner number = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int num = number.nextInt();
-
-        if (num > 0) {
-            System.out.println("The Number is positive");
-        } else if (num < 0) {
-            System.out.println("The number is negative");
-        } else if (num = String || Float || Double || Byte || Long || Short || Boolean) {
-            System.out.println("The given input is not valid , pls enter a valid input");
-        } else {
-            System.out.println("The number is zero");
-        }
+	// write your code here
     }
 }
